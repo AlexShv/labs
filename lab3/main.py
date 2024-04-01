415ba2077d50101e5d62c3d700c7e571806eeff0
Alex <shvaykalex@gmail.com>
2024-04-01 13:54:50 +0300

    Add table for data, modified function for plot

--- lab3/main.py
+++ lab3/main.py
@@ -2,14 +2,14 @@ from spyre import server
 import pandas as pd
 
 
-class SimpleApp(server.App):
+class App(server.App):
     title = 'NOAA data visualization'
 
     regions_df = pd.read_csv('regions.csv')
     regions_list = regions_df['Region_Name'].tolist()
 
     inputs = [
-        # Випадаючий список для вибору типу даних NOAA
+        # Список для вибору типу даних NOAA
         {
             'type': 'dropdown',
             'label': 'NOAA data',
@@ -29,72 +29,131 @@ class SimpleApp(server.App):
             'key': 'region',
             'action_id': 'update_data',
         },
-        # Текстове поле для введення діапазону місяців
+        # Поле вводу початкового тижня
         {
-            'type': 'text',
-            'label': 'Months interval:',
-            'value': '1-12',
-            'key': 'months_interval',
-            'action_id': 'update_data',
+            "type": 'text',
+            "label": 'Start Week',
+            "key": 'start_week',
+            "value": '1',
+            "action_id": "update_data"
         },
-        # Список для вибору року
+        # Для кінцевого тижня
         {
-            'type': 'dropdown',
-            'label': 'Year',
-            'options': [{'label': str(year), 'value': str(year)} for year in range(1982, 2025)],
-            'key': 'year',
-            'action_id': 'update_data'
+            "type": 'text',
+            "label": 'End Week',
+            "key": 'end_week',
+            "value": '52',
+            "action_id": "update_data"
+        },
+        # Поле для року початку
+        {
+            "type": "text",
+            "label": "Year",
+            "value": "1982",
+            "key": "start_year",
+            "action_id": "update_data",
+        },
+        # Для кінцевого року
+        {
+            "type": "text",
+            "label": "Year",
+            "value": "1982",
+            "key": "end_year",
+            "action_id": "update_data",
         },
     ]
 
-    controls = [{'type': 'button', 'label': 'Build plot', 'id': 'update_data'}]
+    controls = [{"type": "hidden", "id": "update_data"}]
 
-    tabs = ['Plot']
+    tabs = ["Plot", "Table"]
 
     outputs = [
-        # Відображення графіку
         {
-            'type': 'plot',
-            'id': 'plot_id',
-            'control_id': 'update_data',
-            'tab': 'Plot',
+            "type": "plot",
+            "id": "plot_id",
+            "control_id": "update_data",
+            "tab": "Plot",
+        },
+        {
+            "type": "table",
+            "id": "table_id",
+            "control_id": "update_data",
+            "tab": "Table",
+            "on_page_load": True
         },
     ]
 
-    # Завантажую дані із CSV-файлу
+    # Зчитую дані із файлу
     def getData(self, params):
         data = pd.read_csv('data.csv')
+        data = data.dropna(subset=['Year'])
         return data
 
-    # У цій функції фільтрую дані згідно певних критеріїв
-    def filter_data(self, params, year, region, ticker):
-        data = self.getData(params)
-        sorted_data = data.loc[(data['Region_Index'] == int(region)) & (data['Year'] == int(year)), [ticker]]
-        return sorted_data
-
-    # Збираю усі дані для графіку
-    def plot_data(self, filtered_data, region, year, ticker, months_interval):
-        start_month, end_month = map(int, months_interval.split('-'))
-        plt_object = filtered_data.plot()
-        plt_object.set(
-            ylabel="Значення",
-            xlabel="Тиждень",
-            title=f"Графік для області {region}, {year} рік, ряд {ticker}, місяці {start_month}-{end_month}"
-        )
-        figure = plt_object.get_figure()
-        return figure
-
-    # Функція для побудови графіку
+    # Функція для побудови графіків за вказаними даними
     def getPlot(self, params):
-        year = int(params['year'])
-        region = int(params['region'])
-        ticker = params['ticker']
-        months_interval = params['months_interval']
 
-        filtered_data = self.filter_data(params, year, region, ticker)
-        plot = self.plot_data(filtered_data, region, year, ticker, months_interval)
+        region = int(params['region'])
+        start_week = int(params['start_week'])
+        end_week = int(params['end_week'])
+        start_year = int(params['start_year'])
+        end_year = int(params['end_year'])
+        ticker = params['ticker']
+
+        df = self.getData(params)
+        df['Year'] = df['Year'].astype(int)
+
+        if end_year == start_year:
+            data = df[(df['Region_Index'] == region) & (
+                df['Year'] == start_year) & df['Week'].between(start_week, end_week)]
+        elif end_year - start_year > 1:
+            data = df[(df['Region_Index'] == region)]
+            data = data[((data['Year'] == start_year) & (data['Week'] >= start_week)) |
+                        ((data['Year'] == end_year) & (data['Week'] <= end_week)) |
+                        ((data['Year'] > start_year) & (data['Year'] < end_year))]
+        else:
+            data = df[(df['Region_Index'] == region)]
+            data = data[((data['Year'] == start_year) & (data['Week'] >= start_week)) |
+                        ((data['Year'] == end_year) & (data['Week'] <= end_week))]
+        data['Date'] = pd.to_datetime(data.Year.astype(str), format='%Y') + \
+            pd.to_timedelta(data.Week.mul(7).astype(str) + ' days')
+
+        obj = data.plot(x='Date', y=ticker)
+        obj.set_ylabel("NOAA data")
+        obj.set_xlabel("Week/Year")
+        obj.set_title(f"Plot for region №{region}")
+
+        plot = obj.get_figure()
         return plot
 
+    # Функція для відображення таблиці з даними за певний рік
+    def getTable(self, params):
 
-app = SimpleApp()
-app.launch(port=5000)
+        region = int(params['region'])
+        start_year = int(params['start_year'])
+        end_year = int(params['end_year'])
+        start_week = int(params['start_week'])
+        end_week = int(params['end_week'])
+
+        df = self.getData(params)
+
+        if end_week < start_week and start_year >= end_year:
+            return pd.DataFrame({'Error': ['Неправильні дані для Start\\End week']})
+
+        if end_week > 52 or start_week > 52:
+            return pd.DataFrame({'Error': ['Значення для Start\\End week повинні бути <= 52']})
+
+        data = df[df['Region_Index'] == region]
+        data = data[
+            ((data['Year'] == start_year) & (data['Week'] >= start_week) & (data['Year'] < end_year)) |
+            ((data['Year'] == end_year) & (data['Week'] <= end_week) & (data['Year'] > start_year)) |
+            ((data['Year'] == start_year) & (data['Year'] == end_year) & (data['Week'].between(start_week, end_week)))
+            ]
+
+        columns = ['Year', 'Week', params['ticker'], 'Region_Index']
+        data = data.loc[:, columns].copy()
+
+        return data
+
+
+app = App()
+app.launch()
